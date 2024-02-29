25cf5f24ce22debf6ee6411cf2fe73540c797277
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-02-29 10:01:26 +0100

    create Highlight composable

---
+++ src/lib/composables/Highlights.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+let title: string = "Titre de l'affiche beaucoup trop long pour avoir un exemple avec un événement qui abuse de son nom"
+
+import HighlightCard from "../components/HighlightCard.svelte";
+</script>
+
+<main>
+    <div id="highlight" class="w-full bg-yellow-400">
+        <HighlightCard {title} imgSrc="https://picsum.photos/1920/1080" imgTitle="Une img" dates="{['2024-02-15','2024-03-01']}" href="https://perdu.com"/>
+    </div>
+</main>
+
+<style lang="scss">
+    #highlight{
+      height: 600px;
+    }
+</style>
