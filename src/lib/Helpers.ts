0678416df76a3e2fc7d1123a1aeea5316a15d9a9
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-02-29 15:57:45 +0100

    getRandomNumber: Number

--- src/lib/Helpers.ts
+++ src/lib/Helpers.ts
@@ -7,3 +7,9 @@ export function blankable(href: NullableString): NullableString {
 
     return null;
 }
+
+export function getRandomNumber(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
