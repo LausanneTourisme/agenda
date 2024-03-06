88bdb11960603a1cdaef6f973d68559d2f7d95f2
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-03-06 08:40:33 +0100

    delete Loading.svelte

--- src/lib/components/animated/Loading.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import {ClassList} from "../../ClassList";
-    import {fade} from "svelte/transition";
-
-    const style = ClassList.make(['loading loading-ring loading-lg']);
-</script>
-
-
-<span class="{style.merge($$props.class)}" out:fade></span>
