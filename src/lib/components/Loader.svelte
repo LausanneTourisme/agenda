933a956136e4fbfc8b236d8bbe814f28fd7a8148
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-03-06 08:40:47 +0100

    create Loader.svelte

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+import {Moon} from "svelte-loading-spinners";
+import {ClassList} from "../ClassList.js";
+
+let style: ClassList = ClassList.make();
+
+$: style;
+</script>
+
+<div class="{style.merge($$props.class)}" style="{$$props.style}">
+    <Moon size="60" color="#E7302F" unit="px" duration="1s" />
+</div>
