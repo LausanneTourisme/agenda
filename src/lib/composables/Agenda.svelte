cee5ae490350553d34f6a8b515df73a8f93f2e2f
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-03-07 11:36:42 +0100

    using radio button in search-zone

--- src/lib/composables/Agenda.svelte
+++ src/lib/composables/Agenda.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
-    import Heading from "../components/Heading.svelte";
+    import Heading from "$lib/components/Heading.svelte";
+    import DateRangePicker from "$lib/components/DateRangePicker.svelte";
+    import {Calendar} from "lucide-svelte";
 
-    export let events;
+    export let events = [];
 </script>
 
-<div>
+<div class="p-7">
     <Heading tag="h2">Agenda complet</Heading>
-    <div class="search-zone">
-        <button>Aujourd'hui</button>
-        <button>Ce week-end</button>
-        <button>Date</button>
-        <div>
-            <input type="text" placeholder="Rechercher un événement...">
-            <!--            Icon search -->
-        </div>
+
+    <div class="search-zone py-6">
+        <label for="today" class="text-black border border-black hover:border-yellow-400 has-[:checked]:border-yellow-400 hover:bg-yellow-400 has-[:checked]:bg-yellow-400 items-center gap-6 p-4 ring-2 ring-transparent mr-2">
+            Aujourd'hui
+            <input name="search_zone" id="today" value="today" type="radio" class="accent-indigo-500" hidden=true checked>
+        </label>
+        <label for="week-end" class="text-black border border-black hover:border-yellow-400 has-[:checked]:border-yellow-400 hover:bg-yellow-400 has-[:checked]:bg-yellow-400 items-center gap-6 p-4 ring-2 ring-transparent mr-2">
+            Ce Week-end
+            <input name="search_zone" id="week-end" value="week-end" type="radio" class="accent-indigo-500" hidden=true>
+        </label>
+
+        <label for="date" class="text-black border border-black hover:border-yellow-400 has-[:checked]:border-yellow-400 hover:bg-yellow-400 has-[:checked]:bg-yellow-400 items-center gap-6 p-4 ring-2 ring-transparent mr-2">
+                <Calendar class="inline-block mr-2 mb-2"/>
+            <span>
+                Date
+            </span>
+            <input name="search_zone" id="date" value="date" type="radio" class="accent-indigo-500" hidden=true>
+        </label>
     </div>
+<DateRangePicker/>
     <div>
         <button>tag#01</button>
         <button>tag#02</button>
