ffeb567676008b1f8e0f9fe9c88cabd9cb768b2c
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-03-27 14:41:31 +0100

    add clickable to EventCard.svelte

--- src/lib/components/EventCard.svelte
+++ src/lib/components/EventCard.svelte
@@ -9,6 +9,7 @@
     import 'moment/locale/de';
     import TagsSwiper from "$lib/components/TagsSwiper.svelte";
     import {CldImage} from "svelte-cloudinary";
+    import Clickable from "$lib/components/Clickable.svelte";
 
     // trick to bypass error type...
     const key: "fr" | "en" | "de" | "it" | "es" = ($locale ?? "en") as "fr" | "en" | "de" | "it" | "es";
@@ -27,94 +28,100 @@
     const media = event.medias.find(x => x.is_cover)
 </script>
 
-<div class="event-card flex flex-row w-full block bg-slate-100 w-full rounded-sm overflow-hidden {$$props.class}">
-    <div class="image-wrapper aspect-square h-40 sm:h-64">
-        <!--        TODO add placeholder -->
-        {#if media}
-            <CldImage
-                    src="{media.cloudinary_id}"
-                    alt="{media.copyright}"
-                    title="{media.copyright}"
-                    height="{500}"
-                    width="{500}"
-                    sizes="100vw"
-                    class="object-cover bg-gray-300"
-            />
-        {:else}
-            <img src="./TODO_placeholder.png"
-                 alt="image invalide"
-                 title="image invalided"
-                 height="500"
-                 width="500"
-                 class="object-cover"
-            />
-        {/if}
-    </div>
-    <div class="informations p-2 sm:p-4 flex flex-col flex-1 overflow-hidden">
-        <!--TAGS-->
-        <TagsSwiper class="tags w-full sm:mb-3 cursor-pointer"
-                    withArrow="{false}"
-                    withPagination="{false}"
-                    tags="{event.tags}"
-                    tagClass="flex justify-content items-center text-sm sm:text-md sm:pt-1 text-black border border-black rounded-full hover:border-yellow-400 has-[:checked]:border-yellow-400 hover:bg-yellow-400 has-[:checked]:bg-yellow-400 items-center ring-2 ring-transparent px-2 mr-2"/>
+    <div class="event-card flex flex-row w-full block bg-slate-100 w-full rounded-sm overflow-hidden {$$props.class}">
+        <div class="image-wrapper aspect-square h-40 sm:h-64">
 
-        <!--TITLE-->
-        <div class="flex-grow flex items-center">
-            <p class="title mt-3 mb-1 ml-1 line-clamp-2 max-h-12 sm:max-h-14 text-md sm:text-xl leading-snug tracking-tight font-semibold align-middle"
-               title="{event.seo.name[key]}">
-                {event.seo.name[key]}
-            </p>
+            <Clickable href="{import.meta.env.VITE_LT_URL}{event.seo.hreflang[key]}">
+            <!--        TODO add placeholder -->
+            {#if media}
+                <CldImage
+                        src="{media.cloudinary_id}"
+                        alt="{media.copyright}"
+                        title="{media.copyright}"
+                        height="{500}"
+                        width="{500}"
+                        sizes="100vw"
+                        class="object-cover bg-gray-300"
+                />
+            {:else}
+                <img src="./TODO_placeholder.png"
+                     alt="image invalide"
+                     title="image invalided"
+                     height="500"
+                     width="500"
+                     class="object-cover"
+                />
+            {/if}
+            </Clickable>
         </div>
-        <!--DATE-->
-        <div class="date text-sm sm:text-md flex items-center sm:mt-3">
-            <div class="mb-1 mr-2">
-                <Calendar class="text-honey-500" size="24px"/>
+        <div class="informations flex flex-col flex-1 overflow-hidden">
+            <!--TAGS-->
+            <TagsSwiper class="tags p-2 pb-0 sm:p-4 sm:pb-0  w-full sm:mb-3 cursor-pointer"
+                        withArrow="{false}"
+                        withPagination="{false}"
+                        tags="{event.tags}"
+                        tagClass="flex justify-content items-center text-sm sm:text-md sm:pt-1 text-black border border-black rounded-full hover:border-yellow-400 has-[:checked]:border-yellow-400 hover:bg-yellow-400 has-[:checked]:bg-yellow-400 items-center ring-2 ring-transparent px-2 mr-2"/>
+
+
+            <Clickable class=" px-2 pb-2 sm:px-4 sm:pb-4 flex-grow flex flex-col" href="{import.meta.env.VITE_LT_URL}{event.seo.hreflang[key]}">
+            <!--TITLE-->
+            <div class="flex-grow flex items-center">
+                <p class="title mt-1 mb-1 ml-1 line-clamp-2 max-h-12 sm:max-h-14 text-md sm:text-xl leading-snug tracking-tight font-semibold align-middle"
+                   title="{event.seo.name[key]}">
+                    {event.seo.name[key]}
+                </p>
             </div>
+            <!--DATE-->
+            <div class="date text-sm sm:text-md flex items-center sm:mt-3">
+                <div class="mb-1 mr-2">
+                    <Calendar class="text-honey-500" size="24px"/>
+                </div>
 
-            <p class="flex w-full leading-snug tracking-tight truncate mt-1">
-                {#if isSameDays}
+                <p class="flex w-full leading-snug tracking-tight truncate mt-1">
+                    {#if isSameDays}
                     <span title="{date.start.locale($_('date.locale')).format('DD MMMM YYYY')}">
                         {date.start.locale($_('date.locale')).format('DD.MM.YY')}
                     </span>
-                {:else}
-                    <span class="hidden sm:inline-block pr-1" title="{$_('date.start')}">{$_('date.start')}</span>
+                    {:else}
+                        <span class="hidden sm:inline-block pr-1" title="{$_('date.start')}">{$_('date.start')}</span>
 
-                    <span title="{date.start.locale($_('date.locale')).format('DD MMMM YYYY')}">
+                        <span title="{date.start.locale($_('date.locale')).format('DD MMMM YYYY')}">
                         {date.start.locale($_('date.locale')).format('DD.MM.YY')}
                     </span>
 
-                    <span class="px-1 sm:hidden" title="{$_('date.separator')}"> - </span>
-                    <span class="px-1 hidden sm:inline-block"
-                          title="{$_('date.separator')}"> {$_('date.separator')} </span>
+                        <span class="px-1 sm:hidden" title="{$_('date.separator')}"> - </span>
+                        <span class="px-1 hidden sm:inline-block"
+                              title="{$_('date.separator')}"> {$_('date.separator')} </span>
 
-                    <span title="{date.end.locale($_('date.locale')).format('DD MMMM YYYY')}">
+                        <span title="{date.end.locale($_('date.locale')).format('DD MMMM YYYY')}">
                         {date.end.locale($_('date.locale')).format('DD.MM.YY')}
                     </span>
-                {/if}
-            </p>
-        </div>
-        <!--SCHEDULE-->
-        <div class="schedule text-sm sm:text-md hidden sm:flex w-full sm:mt-4 ">
-            <div class="mr-2">
-                <Clock class="text-honey-500" size="24px"/>
+                    {/if}
+                </p>
             </div>
-            <p class="leading-snug tracking-tight mt-1"
-               title="{$_('date.from')} 17:00">
-                {$_('date.from')} 17:00
-            </p>
-        </div>
-        <!--LOCATION-->
-        <div class="location text-sm sm:text-md flex w-full mt-2 sm:mt-4">
-            <div class="mr-2">
-                <MapPin class="text-honey-500" size="24px"/>
+            <!--SCHEDULE-->
+            <div class="schedule text-sm sm:text-md hidden sm:flex w-full sm:mt-4 ">
+                <div class="mr-2">
+                    <Clock class="text-honey-500" size="24px"/>
+                </div>
+                <p class="leading-snug tracking-tight mt-1"
+                   title="{$_('date.from')} 17:00">
+                    {$_('date.from')} 17:00
+                </p>
             </div>
-            <p class="leading-snug tracking-tight truncate mt-1"
-               title="{geolocation?.venue}">
-                {geolocation?.venue}
-            </p>
+            <!--LOCATION-->
+            <div class="location text-sm sm:text-md flex w-full mt-2 sm:mt-4">
+                <div class="mr-2">
+                    <MapPin class="text-honey-500" size="24px"/>
+                </div>
+                <p class="leading-snug tracking-tight truncate mt-1"
+                   title="{geolocation?.venue}">
+                    {geolocation?.venue}
+                </p>
+            </div>
+            </Clickable>
         </div>
     </div>
-</div>
 
 <style lang="scss">
 </style>
