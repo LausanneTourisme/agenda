4b3dcff42f88826e0506be0858a7942504031e5f
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-04-04 09:04:28 +0200

    add svelte component in addition to web component

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 export * from './App.svelte'
+import './app.scss'
+import App from './App.svelte'
+
+const app = new App({
+    target: document.getElementById('lt-agenda'),
+})
+
+export default app
