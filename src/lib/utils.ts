1e75d3d0a1dc84887328d796617ec6116863256b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-04-18 10:47:03 +0200

    refactor utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,11 @@
-import {type ClassValue, clsx} from "clsx";
-import {twMerge} from "tailwind-merge";
-import {cubicOut} from "svelte/easing";
-import type {TransitionConfig} from "svelte/transition";
+import type {NullableString, Event, GqlOptions, GqlItems, GqlResponse} from "$lib/types";
 
-export const randomString = () => Math.random().toString(36).slice(-8);
+/**
+ * @param size max 36
+ */
+export const randomString = (size: number = 8) => Math.random().toString(36).slice(-size);
+
+export const warn = (information: string, content: any) => console.warn({ information, content })
 
 /**
  *
@@ -13,65 +15,26 @@ export const randomString = () => Math.random().toString(36).slice(-8);
  *
  * @return {number}
  */
-export const randomNumber = (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+export const randomNumber = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+
+export const blankable = (href: NullableString): NullableString => (href && href.includes('http')) ? "_blank" : null;
+
+export async function fetchEvents(url: string | null | undefined, options: GqlOptions): Promise<GqlItems | null> {
+    if (url) {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json");
+
+        const params: URLSearchParams = new URLSearchParams()
+        params.append("options", options.option)
+        params.append("page", options.page?.toString() ?? '1');
+
+        const response: Response = await fetch(`${url}?${params}`, {redirect: 'follow'});
+
+        const gqlResponse = await response.json();
+
+        return gqlResponse?.result?.data?.items;
+    }
+
+    console.error('no api url defined')
+    return null;
 }
-
-/** methods required for popover **/
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
-type FlyAndScaleParams = {
-    y?: number;
-    x?: number;
-    start?: number;
-    duration?: number;
-};
-
-export const flyAndScale = (
-    node: Element,
-    params: FlyAndScaleParams = {y: -8, x: 0, start: 0.95, duration: 150}
-): TransitionConfig => {
-    const style = getComputedStyle(node);
-    const transform = style.transform === "none" ? "" : style.transform;
-
-    const scaleConversion = (
-        valueA: number,
-        scaleA: [number, number],
-        scaleB: [number, number]
-    ) => {
-        const [minA, maxA] = scaleA;
-        const [minB, maxB] = scaleB;
-
-        const percentage = (valueA - minA) / (maxA - minA);
-        const valueB = percentage * (maxB - minB) + minB;
-
-        return valueB;
-    };
-
-    const styleToString = (
-        style: Record<string, number | string | undefined>
-    ): string => {
-        return Object.keys(style).reduce((str, key) => {
-            if (style[key] === undefined) return str;
-            return str + `${key}:${style[key]};`;
-        }, "");
-    };
-
-    return {
-        duration: params.duration ?? 200,
-        delay: 0,
-        css: (t) => {
-            const y = scaleConversion(t, [0, 1], [params.y ?? 5, 0]);
-            const x = scaleConversion(t, [0, 1], [params.x ?? 0, 0]);
-            const scale = scaleConversion(t, [0, 1], [params.start ?? 0.95, 1]);
-
-            return styleToString({
-                transform: `${transform} translate3d(${x}px, ${y}px, 0) scale(${scale})`,
-                opacity: t
-            });
-        },
-        easing: cubicOut
-    };
-};
