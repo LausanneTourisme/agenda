6db7bb57635f684ccafc89e560fa4f71685b6c2c
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-04-18 10:50:09 +0200

    add date-utils

--- src/lib/date-utils.ts
+++ src/lib/date-utils.ts
@@ -1,48 +1,68 @@
-import moment, {type Moment} from "moment";
-import type {Event, optionsSortEvents, Period, ScheduleDate} from "./types";
+import moment, { type Moment } from "moment";
+import type { Event, OptionsSortEvents, Period, ScheduleDate } from "./types";
+import { warn } from "./utils";
 
 export const randomDate = (start: Moment, end: Moment): Moment => {
     return moment(start.valueOf() + Math.random() * (end.valueOf() - start.valueOf()))
 }
 
-export const sortEvents = (events: Event[], options: optionsSortEvents = {
-    onlyAvailable: true,
-    onlyHighlights: false
-}): Event[] => {
-    return events.filter((event: Event) => {
-        if (options.onlyHighlights && !event.highlight) {
+const logIgnoredEvent = (event: Event, information?: string) => warn(`Event skipped${information? `, beacause: ${information}`: null}`, event)
+
+export const sortEvents = (events: Event[], options: OptionsSortEvents): Event[] => {
+    options = {
+        locale: null,
+        onlyAvailable: true,
+        onlyHighlights: false,
+        startingDate: null,
+        endingDate: null,
+        ...options
+    }
+
+    return [...events].filter((e: Event, index: number) => {
+        const event = {...e};
+
+        if(options.locale && !event.languages.includes(options.locale)){
+            logIgnoredEvent(events[index], 'locale not found')
+
             return false;
         }
 
-        const dates: Array<Period[]> = availablePeriods(event);
+        if (event.schedules.dates.length === 0) {
+            logIgnoredEvent(events[index], 'empty dates')
+
+            return false;
+        }
+
+        //when only highlighted event
+        if (options.onlyHighlights && !event.highlight) {
+            logIgnoredEvent(events[index], 'not highlighted')
+            return false;
+        }
 
         if (options.onlyAvailable) {
-            let indexesToDelete = [];
-            for (let i = 0; i < dates.length; i++) {
-                if (dates[i].length === 0) {
-                    indexesToDelete.push(i);
-                } else {
-                    //remove old periods
-                    event.schedules.dates[i].periods = dates[i].sort((d1: Period, d2: Period) => {
-                        const a = moment(d1.start, "YYYY-MM-DD");
-                        const b = moment(d2.start, "YYYY-MM-DD");
+            const availableDates: ScheduleDate[] = []
+            //I love shadow reference ❤️
+            const schedules = {...event.schedules}
 
-                        if (a < b) {
-                            return -1;
-                        } else if (a > b) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    });
-                }
+            for (const date of schedules.dates) {
+                const d = {...date}
+                const period = findAvailablePeriod(d, options.startingDate, options.endingDate);
+                if (!period) continue;
+
+                d.periods = [{...period}];
+                availableDates.push(d);
             }
-            //delete higher index first to prevent calculation
-            indexesToDelete.reverse().forEach(x => event.schedules.dates.splice(x, 1));
 
-            if (event.schedules.dates.length === 0) {
+            if (!availableDates) {
+                logIgnoredEvent(events[index], 'invalid dates')
                 return false
             }
+
+            //prevent references
+            schedules.dates = availableDates
+            event.schedules = schedules;
+
+            return true;
         }
 
         return true;
@@ -50,36 +70,35 @@ export const sortEvents = (events: Event[], options: optionsSortEvents = {
 }
 
 export const uniqueEvents = (arr1: Event[], arr2: Event[]): Event[] => {
-    const result: Event[] = arr2.filter(x => !arr1.find(e => e.id === x.id))
+    const result: Event[] = arr2.filter(x => arr1.find(e => e.id === x.id) === undefined)
 
-    return arr1.concat(result);
+    return [...arr1, ...result];
 }
 
-const availablePeriods = (event: Event): Array<Period[]> => {
-    const dates: Array<Period[]> = event.schedules.dates.map((d: ScheduleDate) => d.periods);
+const findAvailablePeriod = (schedule: ScheduleDate, start: Moment | null | undefined, end: Moment | null | undefined): Period | null => {
+    const today: Moment = start ?? moment();
 
-    let availablePeriods: Array<Period[]> = Array(dates.length).fill([]);
+    for (const period of schedule.periods) {
 
-    const today: Moment = moment();
+        const pStart: Moment = moment(period.start, 'YYYY-MM-DD').startOf('day');
+        const pEnd: Moment = moment(period.end, 'YYYY-MM-DD').endOf('day');
 
-    for (let i: number = 0; i < dates.length; i++) {
-        const periods = dates[i];
-
-        for (const period of periods) {
-            const start: Moment = moment(period.start, 'YYYY-MM-DD').startOf('day');
-            const end: Moment = moment(period.end, 'YYYY-MM-DD').endOf('day');
-
-            if (today.isSame(start, "dates")) {
-                availablePeriods[i].push(period);
-            } else if (today.isSame(end, "dates")) {
-                availablePeriods[i].push(period);
-            } else if (today.isBetween(start, end, "dates", "[]")) {
-                availablePeriods[i].push(period);
-            } else if (today.isBefore(start, "dates")) {
-                availablePeriods[i].push(period);
+        if (today.isSame(pStart, "dates")) {
+            return period;
+        } else if (today.isSame(pEnd, "dates")) {
+            return period;
+        } else if (today.isBetween(start, end, "dates", "[]")) {
+            return period;
+        } else if (today.isBefore(pStart, "dates")) {
+            if (end) {
+                if (end.isBefore(pStart)) {
+                    continue;
+                }
             }
+            return period;
         }
     }
 
-    return availablePeriods;
+    return null;
 }
+
