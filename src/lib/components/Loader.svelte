fbf4857e9a5dc8b080ce89fdf8ba62d6e147a5bb
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-04-25 16:06:49 +0200

    improve loader

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import {Moon} from "svelte-loading-spinners";
+
+    export let size: number = 60;
 </script>
 
-<div class="w-full h-full flex justify-center items-center {$$props.class ?? ''}" style="{$$props.style}">
-    <Moon duration="1s" size="60" unit="px"/>
+<div class="flex justify-center items-center {$$props.class ?? ''}" style="{$$props.style}">
+    <Moon duration="1s" {size} unit="px"/>
 </div>
