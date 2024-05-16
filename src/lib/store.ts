ce7c9df7bf6ec9cb0c077c3b64e53af8a3fad97b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-05-16 15:27:16 +0200

    delete start/end date store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,9 +1,5 @@
 import {type Writable, writable} from "svelte/store";
-import {now} from "$lib/date-utils";
-import type {RawDate} from "$lib/types";
 
 export const blankableLinks: Writable<boolean> = writable<boolean>(true);
 
-export const startDate: Writable<string | RawDate> = writable<string | RawDate>(now);
-export const endDate: Writable<string | undefined | null | RawDate> = writable<string | undefined | null | RawDate>(null);
 
