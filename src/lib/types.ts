df607744ecfff4dcd16c5e92794d2d135e82a96b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-05-29 11:31:42 +0200

    refactor types.ts, thanks @phlisg

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -44,23 +44,29 @@ export type Translatable = {
 }
 
 export type ShortDay = "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
+
 export type Time = {
     start: `${HH}:${mm}`,
     end: `${HH}:${mm}` | null | undefined,
 }
+
 export type Week = {
     days: ShortDay[],
     times: Time[] | null | undefined,
 }
+
 export type RawDate = `${YYYY}-${MM}-${DD}`
+
 export type Period = {
     start: RawDate,
     end: RawDate,
 }
+
 export type Range = {
     from: RawDate,
     to: RawDate,
 }
+
 export type Media = {
     is_cover?: boolean,
     is_in_gallery?: boolean,
@@ -68,15 +74,18 @@ export type Media = {
     original_width?: number,
     copyright?: string,
 }
+
 export type Tag = {
     name: string,
     public_name: Translatable,
 }
+
 export type Category = {
     name: string,
     public_name: Translatable,
     main_category: boolean,
 }
+
 export type Geolocation = {
     zip: string | undefined | null,
     city: string | undefined | null,
@@ -88,6 +97,7 @@ export type Geolocation = {
     longitude: string | undefined | null,
     latitude: string | undefined | null,
 }
+
 export type Seo = {
     noindex: boolean | "true" | "false",
     name: Translatable,
@@ -96,6 +106,7 @@ export type Seo = {
     description: Translatable,
     medias: Media[]
 }
+
 export type Event = {
     id: number,
     languages: Locales,
@@ -122,26 +133,35 @@ export type NullableString = string | null;
 type oneLetter =
     | 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'
     | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
+
 type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
 type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
 type oneToFive = 1 | 2 | 3 | 4 | 5
+
 type zeroToFive = 0 | 1 | 2 | 3 | 4 | 5
+
 /**
  * Years
  */
 type YYYY = `20${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`
+
 /**
  * Months
  */
 type MM = `0${oneToNine}` | `1${0 | 1 | 2}`
+
 /**
  * Days
  */
 type DD = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`
+
 /**
  * Hours
  */
-type HH = `${0 | 1 | 2}${zeroToNine}` | zeroToNine
+type HH = `${0 | 1}${zeroToNine}` | zeroToNine | `20` | `21` |`22` |`23`;
+
 /**
  * Minutes
  */
@@ -157,7 +177,6 @@ export type DispatchApiEvents = CustomEvent<{
     loadMore: { event: any }; //Click event, I've types conflict
     search: { value: string };
 }>;
-
 
 export type History = {
     load_by: number, //how many events do you want load on every "load more"
@@ -200,6 +219,8 @@ export type History = {
     },
     has_more: boolean,
 };
+
+export type Query = string | undefined | null;
 
 export type GqlOptions = {
     options: "search" | "getEvents" | "getHighlights" | "getevents" | "gethighlights" | 'dates' | 'tags'
