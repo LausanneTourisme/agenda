9224244eaaf6e7b89725a59bc20a9a3bfe4a7a3f
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-05-29 14:58:38 +0200

    fix missing font family euclid flex

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,7 +72,8 @@ const config = {
                 sm: "calc(var(--radius) - 4px)"
             },
             fontFamily: {
-                sans: [...fontFamily.sans]
+                sans: [...fontFamily.sans],
+                euclid: ['Euclid\\ Flex', "sans-serif"]
             }
         }
     },
