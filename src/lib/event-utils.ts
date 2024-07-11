687819ce1e826800b709801e8941cba0d240783b
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-07-11 13:54:31 +0200

    fix event-utils

--- src/lib/event-utils.ts
+++ src/lib/event-utils.ts
@@ -16,24 +16,6 @@ import Fuse from "fuse.js";
 
 export const logIgnoredEvent = (event: Event, information?: string): void | null => warn(`Event skipped${information ? `, beacause: ${information}` : null}`, event)
 
-export const update = async (url: string | undefined | null, locale: Locales, type: EventType = EventType.agenda, events: Event[] = [], limit: number | null = null): Promise<EventsResult> => {
-    if (!url) throw new Error("Api url not configured")
-
-    const items = await fetchEvent(url, {
-        options: `get${type}`,
-        ignoreIds: events?.map(e => e.id),
-        limit,
-        locale,
-    });
-
-    const elements = items?.data ?? [];
-
-    return {
-        // @ts-ignore
-        hasMore: items?.has_more_pages ?? items?.data?.length > 0,
-        events: elements,
-    }
-}
 
 export const sort = (events: Event[], options: OptionsSortEvents = {}): Event[] => {
     options = {
@@ -124,142 +106,6 @@ export const unique = (arr1: Event[], arr2: Event[]): Event[] => {
     return [...arr1, ...result];
 }
 
-export const fetchAll = async (url: string | null | undefined): Promise<Event[] | null> => {
-    if (!url) {
-        console.error('no api url defined')
-        return null;
-    }
-
-    // @ts-ignore
-    const response: Response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        body: JSON.stringify({
-            options: 'dumpEvents',
-        }),
-    });
-
-    const gqlResponse = await response.json() as GqlResponse;
-
-    return gqlResponse?.result?.data?.items.data ?? [];
-}
-
-export const fetchEvent = async (url: string | null | undefined, options: GqlOptions): Promise<GqlItems | null> => {
-    if (!url) {
-        console.error('no api url defined')
-        return null;
-    }
-
-    const response: Response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        redirect: 'follow',
-        body: JSON.stringify(options),
-    });
-
-    const gqlResponse = await response.json();
-
-    return gqlResponse?.result?.data?.items;
-}
-
-// @ts-ignore
-export const getFreshEvents = async (apiUrl: string | null | undefined, locale: Locales, events: Event[], options: {
-    disableAgenda?: boolean,
-    disableHighlights?: boolean,
-    events_per_chunk?: number,
-} = {}): Promise<{
-    agenda: Event[],
-    highlights: Event[],
-    events: Event[],
-    usableEvents: Event[],
-}> => {
-    if (!apiUrl) {
-        console.error('getFreshEvent: no api url defined');
-        return {agenda: [], highlights: [], events: [], usableEvents: []};
-    }
-
-    options = {disableAgenda: false, disableHighlights: false, events_per_chunk: 10, ...options}
-
-    const emptyEvents = events.length === 0;
-    let highlights: Event[] = [];
-    let agenda: Event[] = [];
-    let newEvents: Event[] = [];
-    let usableEvents: Event[] = [];
-
-    log('freshEvents: ', {emptyEvents})
-
-    if (emptyEvents) {
-        if (!options.disableHighlights) {
-            const type: EventType = EventType.highlights;
-
-            const result: EventsResult = await update(apiUrl, locale, type, [], options.events_per_chunk);
-            log('getFreshEvent: getting highlights', {highlights: result.events})
-
-            highlights = [...result.events];
-        }
-        if (!options.disableAgenda) {
-            const type: EventType = EventType.agenda;
-
-            const result: EventsResult = await update(apiUrl, locale, type, highlights, options.events_per_chunk);
-            log('getFreshEvent: getting agenda events', {agenda: result.events})
-
-            agenda = [...result.events, ...highlights];
-        }
-
-        newEvents = [];
-
-        usableEvents = sort([...highlights, ...agenda]);
-
-        log('getFreshEvent: events completely sorted', {newEvents, usableEvents})
-    } else {
-        log('getFreshEvent: events already exists let we use it')
-
-        usableEvents = events.filter(event => event.languages.includes(locale));
-        const tempHighlights: Event[] = [];
-        const tempAgenda: Event[] = [];
-        let index: number = 0;
-
-        for (const event of usableEvents) {
-            // @ts-ignore typescript can't see default value defined bellow
-            if (event.highlight && tempHighlights.length < options.events_per_chunk) {
-                tempHighlights.push(event);
-                tempAgenda.push(event);
-                //@ts-ignore
-            } else if (index < options.events_per_chunk && !event.highlight) {
-                tempAgenda.push(event);
-                index++;
-                //@ts-ignore
-            } else if (index >= options.events_per_chunk && tempHighlights.length >= options.events_per_chunk) {
-                break;
-            }
-        }
-        highlights = [...tempHighlights];
-        agenda = [...tempAgenda];
-        newEvents = [...events];
-    }
-
-    return {agenda, highlights, events: newEvents, usableEvents};
-}
-
-export async function getAllEvents(apiUrl: string): Promise<Event[]> {
-    log('getAllEvents: getting all events')
-    const items = await fetchAll(apiUrl);
-    log('getAllEvents: dump completed', {events: items})
-
-    if (!items) return [];
-
-    return items;
-}
-
 export const searchEvents = (query: Query, locale: Locales = "en", events: Event[] = []) => {
     if (!query || events.length === 0) {
         return [...events];
@@ -302,3 +148,162 @@ export const searchEvents = (query: Query, locale: Locales = "en", events: Event
 
     return [...fuse.search(query.toLowerCase()).map(e => e.item)];
 };
+
+// @ts-ignore
+export const getFreshEvents = async (apiUrl: string | null | undefined, locale: Locales, events: Event[], options: {
+    disableAgenda?: boolean,
+    disableHighlights?: boolean,
+    events_per_chunk?: number,
+} = {}): Promise<{
+    agenda: Event[],
+    highlights: Event[],
+    events: Event[],
+    usableEvents: Event[],
+}> => {
+
+    if (!apiUrl) {
+        console.error('getFreshEvent: no api url defined');
+        return {agenda: [], highlights: [], events: [], usableEvents: []};
+    }
+
+    options = {disableAgenda: false, disableHighlights: false, events_per_chunk: 10, ...options}
+
+
+    const emptyEvents = events.length === 0;
+    let highlights: Event[] = [];
+    let agenda: Event[] = [];
+    let newEvents: Event[] = [];
+    let usableEvents: Event[] = [];
+
+
+    log('Has to get freshEvents: ', {emptyEvents});
+
+
+    if (emptyEvents) {
+        if (!options.disableHighlights) {
+            const type: EventType = EventType.highlights;
+
+            const result: EventsResult = await update(apiUrl, locale, type, [], options.events_per_chunk);
+            log('getFreshEvent: getting highlights', {highlights: result.events})
+
+            highlights = [...result.events];
+        }
+        if (!options.disableAgenda) {
+            const type: EventType = EventType.agenda;
+
+            const result: EventsResult = await update(apiUrl, locale, type, highlights, options.events_per_chunk);
+            log('getFreshEvent: getting all type of events', {agenda: result.events})
+
+            agenda = [...result.events, ...highlights];
+        }
+
+        usableEvents = sort([...highlights, ...agenda]);
+        newEvents = []
+        log('getFreshEvent: events completely sorted', {totalEvents: newEvents, usableEvents})
+    } else {
+        log('getFreshEvent: events already exists let we use it')
+
+        usableEvents = events.filter(event => event.languages.includes(locale));
+        const tempHighlights: Event[] = [];
+        const tempAgenda: Event[] = [];
+        let index: number = 0;
+
+        for (const event of usableEvents) {
+            // @ts-ignore typescript can't see default value defined bellow
+            if (event.highlight && tempHighlights.length < options.events_per_chunk) {
+                tempHighlights.push(event);
+                tempAgenda.push(event);
+                //@ts-ignore
+            } else if (index < options.events_per_chunk && !event.highlight) {
+                tempAgenda.push(event);
+                index++;
+                //@ts-ignore
+            } else if (index >= options.events_per_chunk && tempHighlights.length >= options.events_per_chunk) {
+                break;
+            }
+        }
+        highlights = [...tempHighlights];
+        agenda = [...tempAgenda];
+        newEvents = [...events];
+    }
+
+    return {agenda, highlights, events: newEvents, usableEvents};
+};
+
+
+export const update = async (url: string | undefined | null, locale: Locales, type: EventType = EventType.agenda, events: Event[] = [], limit: number | null = null): Promise<EventsResult> => {
+    if (!url) throw new Error("Api url not configured")
+
+    const items = await fetchEvent(url, {
+        options: `get${type}`,
+        ignoreIds: events?.map(e => e.id),
+        limit,
+        locale,
+    });
+
+    const elements = items?.data ?? [];
+
+    return {
+        // @ts-ignore
+        hasMore: items?.has_more_pages ?? items?.data?.length > 0,
+        events: elements,
+    }
+}
+
+
+export const fetchEvent = async (url: string | null | undefined, options: GqlOptions): Promise<GqlItems | null> => {
+    if (!url) {
+        console.error('no api url defined')
+        return null;
+    }
+
+    const response: Response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify(options),
+    });
+
+    const gqlResponse = await response.json();
+
+    return gqlResponse?.result?.data?.items;
+}
+
+export const fetchAll = async (url: string | null | undefined): Promise<Event[] | null> => {
+    if (!url) {
+        console.error('no api url defined')
+        return null;
+    }
+
+    // @ts-ignore
+    const response: Response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({
+            options: 'dumpEvents',
+        }),
+    });
+
+    const gqlResponse = await response.json() as GqlResponse;
+
+    return gqlResponse?.result?.data?.items.data ?? [];
+}
+
+export async function getAllEvents(apiUrl: string): Promise<Event[]> {
+    log('getAllEvents: getting all events')
+    const items = await fetchAll(apiUrl);
+    log('getAllEvents: dump completed', {events: items})
+
+    if (!items) return [];
+
+    return items;
+}
