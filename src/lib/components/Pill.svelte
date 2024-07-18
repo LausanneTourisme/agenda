c3e2aa79650acdd38d4b79c8e8cda079d1bf2af9
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2024-07-18 14:02:31 +0200

    create new component pill

---
+++ src/lib/components/Pill.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let title: string;
+
+    $: $$props.class;
+</script>
+
+<button on:click
+        class={$$props.class}
+        title="{title}">
+    {title}
+</button>
